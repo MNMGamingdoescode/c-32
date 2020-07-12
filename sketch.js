46e408d4dab6d8253dd20b1c24372e7efd1157b7
MNMGamingdoescode <64952388+MNMGamingdoescode@users.noreply.github.com>
2020-07-12 16:30:38 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,119 +1,42 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+var canvas, backgroundImage;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
 
-var gameState = "onSling";
-var bg = "sprites/bg1.png";
-var score = 0;
+var form, player, game;
+
+var runners, runner1, runner2, runner3, runner4;
+
+var track, runner1_img, runner2_img, runner3_img, runner4_img, hurdle, invisibleGround1, invisibleGround2;
 
 function preload() {
-    getBackgroundImg();
+    hurdle = loadImage("../images/hurdle.png");
+    track = loadImage("../images/pla.jpg");
+    runner1_img = loadImage("b.png", "p.png", "y.png");
+    runner2_img = loadImage("b.png", "p.png", "y.png");
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
-}
-
-function draw(){
-    if(backgroundImg)
-        background(backgroundImg);
-    
-        noStroke();
-        textSize(35);
-        fill("white");
-        text("Score  " + score, width-300, 50);
-    
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    pig1.score();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    pig3.score();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
-}
-
-function mouseDragged(){
-    //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+function setup() {
+    canvas = createCanvas(displayWidth, window.innerHeight);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
 }
 
 
-function mouseReleased(){
-    slingshot.fly();
-    gameState = "launched";
-}
-
-function keyPressed(){
-    if(keyCode === 32 && bird.body.speed <1){
-       bird.trajectory = [];
-       Matter.Body.setPosition(bird.body,{x:200,y:50});
-       slingshot.attach(bird.body);
+function draw() {
+    if (playerCount === 2) {
+        game.update(1);
     }
-}
-
-async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    var responseJSON = await response.json();
-
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
-    
-    if(hour>=0600 && hour<=1900){
-        bg = "bg.png";
+    if (gameState === 1) {
+        clear();
+        game.play();
     }
-    else{
-        bg = "bg2.jpg";
+    if (gameState === 2) {
+        game.end();
     }
-
-    backgroundImg = loadImage(bg);
-    console.log(backgroundImg);
 }
